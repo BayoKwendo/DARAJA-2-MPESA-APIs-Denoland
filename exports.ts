ec3107cd8e5adb69fba4c69b47367ad0838e0479
bayokwendo <kwendobrian96@gmail.com>
2021-10-07 12:34:36 +0300

    removed unnecessary files

--- exports.ts
+++
@@ -1,8 +0,0 @@
-export const  key = await crypto.subtle.generateKey({ name: 'HMAC', hash: 'SHA-512' }, true, [ 'verify', 'sign' ]);
-
-
-// const key = await crypto.subtle.generateKey(
-//     { name: "HMAC", hash: "SHA-512" },
-//     true,
-//     ["sign", "verify"],
-//     );
--- interfaces/mpesa.ts
+++ interfaces/mpesa.ts
@@ -2,8 +2,8 @@
 // our interface/modal
 export default interface Loan {
   id?: number,
-  page_number?:number,
-  offset?:number,
-  page_size?: number,
-  filter_value?:any,
+  // page_number?:number,
+  // offset?:number,
+  // page_size?: number,
+  // filter_value?:any,
 } 
